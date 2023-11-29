a3e257a98061d8cb897fbe8d92ba280931065a67
Sai Deepak <sai@livewd.ca>
2023-11-30 01:17:23 +0530

    html to php

---
+++ _include/top_navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg sticky-top">
+    <div class="container"><a class="navbar-brand d-flex align-items-center" href="index.html"><span><img class="brand-logo" src="assets/img/logo.png?h=1fd40d2a4ed2e00638b7770c544e1dd0"></span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle
+                navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navcol-2">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="sellers.html">Sellers</a></li>
+                <li class="nav-item"><a class="nav-link" href="buyers.html">buyers</a></li>
+                <li class="nav-item"><a class="nav-link" href="invest.html">invest</a></li>
+                <li class="nav-item"><a class="nav-link" href="developments.html">developments</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.html">about</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.html">contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ contact.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <script>
+    var elements = document.querySelectorAll('.property-text');
+
+    elements.forEach(function(element) {
+      var text = element.textContent;
+      if (text.length > 300) {
+        element.textContent = text.slice(0, 300) + '...';
+      }
+    });
+  </script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>Contact</title>
+  <link rel="canonical" href="https://gobeyondrealestate.com/contact.html">
+  <meta property="og:url" content="https://gobeyondrealestate.com/contact.html">
+  <meta name="twitter:description" content="Explore the Fraser Valley with Beyond Visscher, your dedicated realtor. With a rich background of global adventures, we offer personalized real estate services. Find your dream home with us.">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta property="og:type" content="website">
+  <meta name="description" content="Welcome to Beyond Visscher - Your Trustworthy Realtor in the Fraser Valley! Explore the Fraser Valley with Beyond Visscher, your dedicated realtor. With a rich background of global adventures, we offer personalized real estate services. Find your dream home with us.">
+  <meta property="og:image" content="https://gobeyondrealestate.com/assets/img/facebook-card-preview-1200x630.jpg">
+  <meta name="twitter:title" content="Beyond Visscher | Century 21 - Go Beyond Real Estate">
+  <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="assets/img/apple-touch-icon.png?h=1d08c77f8efc13d2cc7e0b282f0371a2">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png?h=1d08c77f8efc13d2cc7e0b282f0371a2">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png?h=1d08c77f8efc13d2cc7e0b282f0371a2" media="(prefers-color-scheme: dark)">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png?h=1d08c77f8efc13d2cc7e0b282f0371a2">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png?h=1d08c77f8efc13d2cc7e0b282f0371a2" media="(prefers-color-scheme: dark)">
+  <link rel="icon" type="image/png" sizes="180x180" href="assets/img/apple-touch-icon.png?h=1d08c77f8efc13d2cc7e0b282f0371a2">
+  <link rel="icon" type="image/png" sizes="180x180" href="assets/img/apple-touch-icon.png?h=1d08c77f8efc13d2cc7e0b282f0371a2">
+  <link rel="icon" type="image/png" sizes="512x512" href="assets/img/android-chrome-512x512.png?h=1d08c77f8efc13d2cc7e0b282f0371a2">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
+  <link rel="stylesheet" href="assets/css/styles.min.css?h=bdf7da12a335af1c2a1064825b114da2">
+</head>
+
+<body><!-- Start: Navbar Right Links -->
+  <nav class="navbar navbar-expand-lg sticky-top">
+    <div class="container"><a class="navbar-brand d-flex align-items-center" href="index.html"><span><img class="brand-logo" src="assets/img/logo.png?h=1fd40d2a4ed2e00638b7770c544e1dd0"></span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navcol-2">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-link" href="sellers.html">Sellers</a></li>
+          <li class="nav-item"><a class="nav-link" href="buyers.html">buyers</a></li>
+          <li class="nav-item"><a class="nav-link" href="invest.html">invest</a></li>
+          <li class="nav-item"><a class="nav-link" href="developments.html">developments</a></li>
+          <li class="nav-item"><a class="nav-link" href="about.html">about</a></li>
+          <li class="nav-item"><a class="nav-link" href="contact.html">contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav><!-- End: Navbar Right Links -->
+  <section class="innerpage-main p-0">
+    <div class="container-fluid p-0">
+      <div class="row g-0">
+        <div class="col-12 d-flex justify-content-center align-items-center col-lg-4 text-center pt-5 pb-5">
+          <div>
+            <h5 class="text-success fw-bold">Let's stay connected.</h5>
+            <h1 class="f-noyh pb-0">BEYOND VISSCHER</h1>
+            <h5 class="f-noyh">CENTURY 21 CREEKSIDE REALTY</h5>
+            <ul class="list-unstyled text-start pt-3">
+              <li><i class="fas fa-phone-alt contact-icon"></i>&nbsp;604.368.0512</li>
+              <li class="pt-2"><i class="fas fa-envelope contact-icon"></i>&nbsp;beyond@gobeyondrealestate.com</li>
+              <li class="pt-2"><i class="fas fa-map-marker-alt contact-icon"></i>&nbsp;102-7075 Vedder Road<span class="display-only-nonmobile">, Chilliwack BC V2R 09X</span></li>
+              <li class="display-only-mobile">Chilliwack BC V2R O9X</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-12 col-lg-8 mh-500"><iframe allowfullscreen="" frameborder="0" loading="lazy" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDGA8m_CZJac2qmxd1V8y3ZBp7d85FPaIU&amp;q=102-7075+Vedder+Road%2C+Chilliwack+BC+V2R+O9X&amp;zoom=16" width="100%" height="100%"></iframe></div>
+      </div>
+    </div>
+  </section>
+  <section class="innerpage-main bg-primary"><!-- Start: Testimonials -->
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md-8 col-xl-6 text-center mx-auto">
+          <h1 class="f-noyh">TESTIMONIALS</h1>
+        </div>
+      </div>
+      <div class="row gy-4 row-cols-1 row-cols-sm-2 row-cols-lg-3">
+        <div class="col">
+          <div>
+            <p class="bg-body-tertiary border rounded border-0 p-4 testimonial-text">Beyond is an amazing and talented real estate agent and I would recommend her to anyone looking to purchase a home in the Fraser Valley.&nbsp;We purchased a rental property through Beyond that closed in November 2020 and she was exceptional in how she was able to pair our specific purchase requirements to what was available in our target market.&nbsp;Thank you so much Beyond, and we look forward to doing business with you in the future.</p>
+            <div class="d-flex">
+              <div>
+                <p class="fw-bold testimonial-user">Rick Giesbrecht</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div>
+            <p class="bg-body-tertiary border rounded border-0 p-4 testimonial-text">Working with Beyond has been an absolute pleasure. She is a model realtor, and demonstrated professionalism and honesty throughout this entire process.<br><br>I decided on very short notice to try and sell my condo. If it wasn't for Beyond none of this could have happened as smoothly as it did. Despite there being lots of competition in my area, we sold for a good price and the process was finished very quickly.</p>
+            <div class="d-flex">
+              <div>
+                <p class="fw-bold testimonial-user">Isaac</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div>
+            <p class="bg-body-tertiary border rounded border-0 p-4 testimonial-text">Working with Beyond was such a delight! She was very motivated and diligent in finding what was most suitable for us. She took her time to listen and spent time making sure our needs were met. She sold our house and helped us buy one within a weeks time with no hassle or issues. She also provided moving boxes in her services which was such a huge help. It made moving go smoothly. Would highly recommend her to anyone!</p>
+            <div class="d-flex">
+              <div>
+                <p class="fw-bold testimonial-user">Leah Kerkhoff</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div>
+            <p class="bg-body-tertiary border rounded border-0 p-4 testimonial-text">Beyond is such a professional and hard work agent. I will highly recommend her for everyone. She sold our townhouse within one week with a great price, and she did all the cleaning job, which I really appreciate it. She also helped us get our “perfect” new house, even we have so many critical needs. She is not only an agent, but also a “life-saver” for our whole family.</p>
+            <div class="d-flex">
+              <div>
+                <p class="fw-bold testimonial-user">Yuki Dong</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div>
+            <p class="bg-body-tertiary border rounded border-0 p-4 testimonial-text">Beyond is a very professional lady. I was so luck to have her as my realtor .&nbsp;I am very much impressed by her own&nbsp;heart touching way to do business . Thoughtfulness,&nbsp;highest efficiency ,and overall good quality of her service would definitely make me recommend her to my friends. Thank you very much Beyond!🙏🌹❤️</p>
+            <div class="d-flex">
+              <div>
+                <p class="fw-bold testimonial-user">Shangyang Tang</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div>
+            <p class="bg-body-tertiary border rounded border-0 p-4 testimonial-text">Beyond helped me purchase my first home recently, and she was amazing! She pays attention to what you like and don't like and suggests options accordingly. She really went the extra mile to make the process as smooth as possible and make me feel at home in my new place. I recommend her to anyone looking to buy or sell!!</p>
+            <div class="d-flex">
+              <div>
+                <p class="fw-bold testimonial-user">Elizabeth Campbell</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div>
+            <p class="bg-body-tertiary border rounded border-0 p-4 testimonial-text">Beyond is one of the best realtors I have worked with. She used her professional team to clean and stage my property before listing , and stayed in touch and assisted in making the home for sale while I was living in another city from the home to be sold. Her knowledge of the Chilliwack area was very useful. She went the extra mile to keep my property presentable, for example: cutting the grass, cleaning driveway, etc. After my property was sold she assisted me with the moving which was a big help for us. Her assistance through the whole process was very pleasant . I will highly recommend Beyond to all my friends.</p>
+            <div class="d-flex">
+              <div>
+                <p class="fw-bold testimonial-user">Ruby S</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div>
+            <p class="bg-body-tertiary border rounded border-0 p-4 testimonial-text">I had the pleasure of working on a deal with Beyond. She is extremely professional and definitely worked hard to bring it all together. Having a good relationship with all parties involved makes for a great experience and she made sure to do just that. She does her homework and was always fully prepared. She was very accommodating and I would definitely recommend Beyond. Thank you for everything and I look forward to working with you again.</p>
+            <div class="d-flex">
+              <div>
+                <p class="fw-bold testimonial-user">Themy Gitersos</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div>
+            <p class="bg-body-tertiary border rounded border-0 p-4 testimonial-text">I've utilized Beyond purchasing properties in the Fraser Valley for many years with great return &amp; solid experience in real estate market.<br><br>Beyond is very skilled and a strategic negotiator, she skilled fully worked with us on our house sale in Chilliwack.<br><br>Thank you Beyond!</p>
+            <div class="d-flex">
+              <div>
+                <p class="fw-bold testimonial-user">Jenny Zhao</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- End: Testimonials -->
+  </section>
+  <section class="innerpage-main">
+    <div class="container">
+      <h1>"IS PROPERTY INVESTMENT RIGHT FOR ME?"</h1>
+      <h3>Beyond values and cares for every client, so you can expect to receive honest advice that has your long term good in mind. Click an article below to learn more.</h3>
+      <div class="row g-0 articles-row">
+        <div class="col-12 col-md-4 col-lg-4">
+          <div class="article-showcase-div"><!-- Start: article-img-showcase -->
+            <div><img class="img-fluid" src="assets/img/invest-1.jpg?h=7041eefbe13272cdaebd5828e37c54fd"></div><!-- End: article-img-showcase -->
+            <h5>Secure Your Children’s Future Through Real Estate</h5><a class="readmore" href="#">read more</a>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 col-lg-4">
+          <div class="article-showcase-div"><!-- Start: article-img-showcase -->
+            <div><img class="img-fluid" src="assets/img/invest-2.jpg?h=1be7692ce1219939468b4585ab2e6cf8"></div><!-- End: article-img-showcase -->
+            <h5><strong>Why Business Owners Should Invest in Real Estate?</strong></h5><a class="readmore" href="#">read more</a>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 col-lg-4">
+          <div class="article-showcase-div"><!-- Start: article-img-showcase -->
+            <div><img class="img-fluid" src="assets/img/invest-3.jpg?h=aedc69a2de114e6a839f39cd03856223"></div><!-- End: article-img-showcase -->
+            <h5><strong>Why Real Estate is Your Best Retirement Plan?</strong></h5><a class="readmore" href="#">read more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- Start: newsletter -->
+  <section class="newsletter-section">
+    <div class="container">
+      <div class="row g-0 pt-3">
+        <div class="col-lg-7">
+          <h1>SIGN UP FOR OUR NEWSLETTER</h1>
+          <h6>Stay in the know on real estate. Subscribe for updates and tips.</h6>
+        </div>
+        <div class="col">
+          <div>
+            <div class="row g-0">
+              <div class="col-8 text-end"><input type="email" class="email-input" autocomplete="off" required="" placeholder="youraddress@mail.com"></div>
+              <div class="col-2"><input class="btn btn-success subscribe-btn" type="submit" value="Subscribe"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- End: newsletter --><!-- Start: footer -->
+  <section class="text-center footer">
+    <div class="container">
+      <div class="row pb-5">
+        <div class="col-12 col-md-6 col-lg-2 footer-col-1"><img class="img-fluid" src="assets/img/beyond-logo-white-footer.png?h=87ebb891c57d8c1ad716fb09b49036a1"></div>
+        <div class="col-12 col-md-6 col-lg-5 footer-col-2">
+          <ul class="list-unstyled text-start text-white">
+            <li class="footer-text"><i class="fas fa-phone-alt footer-icon"></i>&nbsp;604.368.0512</li>
+            <li class="footer-text pt-2"><i class="fas fa-envelope footer-icon"></i>&nbsp;beyond@gobeyondrealestate.com</li>
+            <li class="footer-text pt-2"><i class="fas fa-map-marker-alt footer-icon"></i>&nbsp;102-7075 Vedder Road<span class="display-only-nonmobile">, Chilliwack BC V2R 09X</span></li>
+            <li class="display-only-mobile">Chilliwack BC V2R O9X</li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3 footer-col-3">
+          <h5 class="text-white footer-text">English &amp; Mandarin Service</h5>
+          <h6 class="footer-text text-white">中文服务 | 地产买卖 | 投资开发</h6>
+        </div>
+        <div class="col-12 col-md-6 col-lg-2 footer-col-4"><img class="img-fluid c21" src="assets/img/beyond-logo-C21.png?h=50e0ae624caffd83717773883ea74c7f" width="250px" height=""></div>
+      </div>
+    </div>
+    <p>Terms of Service | Privacy Policy</p>
+    <p>2023&nbsp;© Copyrights to Go Beyond Real estate. All Rights Reserved.</p>
+    <p class="powered-by">Website by <a href="https://livewd.ca" target="_blank">Live Web Design</a></p>
+  </section><!-- End: footer -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
